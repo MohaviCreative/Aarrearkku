18c758e5046376100695d6f0233211368ed05a65
Severi Jokiperä <severi.jokipera@gmail.com>
2020-06-04 13:34:29 +0300

    Core game loop done
    Took most of three.js out, replaced with pure javascript

--- js/main.js
+++ js/main.js
@@ -1,145 +1,136 @@
-var scene = new THREE.Scene();
-var multiplier = 100;
+const multiplier = 100;
 var screenSize = new THREE.Vector2(0.9, 0.9);
-var camera = new THREE.OrthographicCamera( 0, 0, 0, 0, 1, 1000 );
-var resizeImages = true;
-var mouse = new THREE.Vector2();
-var raycaster = new THREE.Raycaster();
-var canvas = document.getElementById("canvas");
-var renderer = new THREE.WebGLRenderer( { canvas: canvas } );
 var border;
-scene.background = new THREE.Color( "rgb(255, 228, 225)" );
-document.body.appendChild( renderer.domElement );
+var random = [];
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+var parent = document.createElement("div");
+body.append(parent);
+parent.style.position = "absolute";
+parent.style.backgroundColor = "blue";
+
+body.style.overflowY= "hidden";
+body.style.overflowX= "hidden";
+var chosen;
+var animationX=0;
+var animation;
 
 
 var mouseDown = false;
+/*
 document.addEventListener( 'mousedown', OnMouseDown, false );
 document.addEventListener( 'mouseup', OnMouseUp, false );
 document.addEventListener( 'mousemove', OnMouseMove, false );
-var text;
-CreateText();
+*/
 
-var listener = new THREE.AudioListener();
-camera.add( listener );
 
 
 var currentSound = 0;
 var sounds = [];
 CreateAudios();
 
+var texts = [];
+CreateTexts();
 
-
-camera.position.z = 5;
-
-window.onresize = Resize;
 
 var sprite = [];
+var images = [];
+CreateImages();
 
-var spriteMap = [
-    new THREE.TextureLoader().load( "Art/Eläimet/Hämähäkki_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Hamsteri_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Hevonen_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Hiiri_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Käärme_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Kala_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Kani_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Kissa_väri.jpg" )/*,
-    new THREE.TextureLoader().load( "Art/Eläimet/Koira_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Lintu_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Lisko_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Marsu_väri.jpg" ),
-    new THREE.TextureLoader().load( "Art/Eläimet/Rotta_väri.jpg" )*/
-]
+const optionsAmount = Math.min(6, sprite.length);
 
-for(i = 0; i < spriteMap.length; i++)
-{
-    sprite.push(new THREE.Sprite(new THREE.SpriteMaterial( { map: spriteMap[i] } )));
-    sprite[i].scale.set(2, 2, 1);
-    sprite[i].position.set(0, 0, 0);
-    scene.add( sprite[i] );
-}
-
-animate();
+ChooseRandom();
 Resize();
+
+
+window.onload = function()
+{
+    window.onresize = Resize;
+}
 
 function Resize()
 {
-    resizeImages = true;
     //renderer.setSize( window.innerWidth * screenSize.x, window.innerHeight * screenSize.y );
 
 
-    var w = new THREE.Vector2(window.innerWidth, window.innerHeight);
-    var wSize = new THREE.Vector2(w.x, w.y).multiply(screenSize);
+    const w = new THREE.Vector2(window.innerWidth, window.innerHeight);
+    const wSize = new THREE.Vector2(w.x, w.y).multiply(screenSize);
     border = new THREE.Vector2(1 - screenSize.x, 1 - screenSize.y)
     border.multiply(w);
     border.divideScalar(2);
 
-    renderer.setSize( wSize.x, wSize.y,false );
-    canvas.style.position = "absolute";
-    canvas.style.left = border.x;
-    canvas.style.top = border.y;
+    parent.style.width = wSize.x; 
+    parent.style.height = wSize.y;
+    parent.style.left = border.x;
+    parent.style.top = border.y;
 
-    camera.left = window.innerWidth / -multiplier;
-    camera.right = window.innerWidth / multiplier;
-    camera.top = window.innerHeight / multiplier;
-    camera.bottom = window.innerHeight / -multiplier;
+    const maxCell = optionsAmount;
+    const lines = parent.offsetWidth < parent.offsetHeight ? 2 : 1;
+    const columns = Math.min(sprite.length, maxCell) / lines;
+    const size = new THREE.Vector2(100 / (columns + 1) - 1, 50);
+    const wholeSpace = 104;
+    var spaceY = 0;
 
-    camera.updateProjectionMatrix ();
-}
+    var plusY = 0;
 
-function animate() 
-{
-    if(resizeImages)
+    if(lines % 2 == 0){
+        spaceY = wholeSpace / 2;
+        plusY=1;
+    }
+
+
+    const test = Math.min(columns,sprite.length - columns)
+
+    for(o = 0; o < lines; o++)
     {
-        resizeImages = false;
+        var spaceX = 0;
 
-        var maxCell = 6;
-        var rows = Math.ceil(spriteMap.length/maxCell);
-        var columns = Math.ceil(spriteMap.length/rows);
-        var size = new THREE.Vector2(100/(columns+1)-1, 100/(rows+1)-1);
+        var plusX = 0;
 
-        for( o = 0; o < rows; o++)
+        if(columns % 2 == 0){
+            spaceX = wholeSpace / 2;
+            plusX=1;
+        }
+
+        var multiplierY = ((o + plusY) % 2 == 0 ? 1 : -1);
+        for(i = 0; i < columns; i++)
         {
-            var test = Math.min(columns,spriteMap.length - (o * columns))
-            var position = new THREE.Vector2(100 / (test + 1), 100 / (rows + 1));
+            var num = i + o * columns;
+            var multiplierX = ((i + plusX) % 2 == 0 ? 1 : -1);
 
-            for(i = 0; i < columns; i++)
-            {
-                if(spriteMap[columns * o + i] && spriteMap[columns * o + i].image)
-                {
-                    ImageScale(sprite[columns * o + i], size);
-                    var testing = new THREE.Vector2(i+1, rows-o);
-                    testing.multiply(position);
-                    ImagePosition(sprite[columns * o + i], testing);
-                }
-                else
-                {
-                    resizeImages = true;
-                    break;
-                }
+
+            ImageScale(random[num], new THREE.Vector2(40,40), sprite);
+            ImagePosition(random[num], new THREE.Vector2(50,25), sprite);
+
+
+            ImageScale(random[num], size, texts);
+            const imageSize = texts[random[num]].offsetWidth / parent.offsetWidth;
+            var testing = new THREE.Vector2( 50 + (spaceX * imageSize * multiplierX), 75 + (spaceY * imageSize * multiplierY));
+            ImagePosition(random[num], testing, texts);
+            EditText(texts[random[num]]);
+
+            if((i + plusX) % 2 == 0){
+                spaceX += wholeSpace;
             }
         }
+        if((o + plusY) % 2 == 0){
+            spaceY += wholeSpace;
+        }
     }
-    
-    if(text!==undefined&&text.material.color!==undefined)
-        RaycastText(text);
-
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
 }
 
-function ImageScale(image, percentage, newSize)
+function ImageScale(i, percentage, array, newSize)
 {
-
     if(newSize == null)
     {
-        newSize = new THREE.Vector2(window.innerWidth,window.innerHeight).multiply(percentage);
-        newSize.divideScalar(50).divideScalar(multiplier);
+        newSize = new THREE.Vector2(parent.offsetWidth, parent.offsetHeight);
+        newSize.multiply(percentage);
+        newSize.divideScalar(multiplier);
     }
 
-    var imageSize = new THREE.Vector2(image.material.map.image.width, image.material.map.image.height);
+    var imageSize = new THREE.Vector2(images[i].width, images[i].height);
     var aspectRatio = new THREE.Vector2(imageSize.y / imageSize.x, imageSize.x / imageSize.y);
-    var aspectSize =new THREE.Vector2(0,0);
+    var aspectSize = new THREE.Vector2(0, 0);
 
 
     aspectSize.y = aspectRatio.x * newSize.x;
@@ -155,63 +146,35 @@ function ImageScale(image, percentage, newSize)
         newSize.x = aspectSize.x;
     }
 
-    image.scale.set(newSize.x, newSize.y);
+    array[i].style.width = newSize.x + "px";
+    array[i].style.height = newSize.y + "px";
 }
 
-function ImagePosition(image, percentage, dynamicPivot)
+function ImagePosition(i, percentage, array)
 {
-    var test = multiplier;
+    var animationSpeed=10;
+    var newSize = new THREE.Vector2(parent.offsetWidth, parent.offsetHeight);
 
-    var positions = new THREE.Vector4(-window.innerWidth, window.innerWidth, -window.innerHeight, window.innerHeight).divideScalar(test);
-    var size = new THREE.Vector2(window.innerWidth, window.innerHeight).divideScalar(multiplier/2);
+    var size = new THREE.Vector2(newSize.x, newSize.y);
+    newSize.divideScalar(2);
 
-    var normalizedSize = new THREE.Vector2(size.x, size.y).multiply(percentage).divideScalar(100);
+    size.multiply(percentage).divideScalar(100);
     var newPosition = new THREE.Vector2(0, 0);
 
-    newPosition.x = normalizedSize.x + positions.x;
+    newPosition.x = parent.offsetLeft + size.x;
 
-    newPosition.y = normalizedSize.y + positions.z;
+    newPosition.y = parent.offsetTop + size.y;
 
-    if(dynamicPivot)
-    {
-        var normalizedSomething = normalizedSize.divide(size);
-        var newPivot = new THREE.Vector2(normalizedSomething.x, normalizedSomething.y);
+    newPosition.x -= array[i].offsetWidth / 2;
+    newPosition.y -= array[i].offsetHeight / 2;
 
-        image.center.set(newPivot.x, newPivot.y);
-    }
+    newPosition.x += animationX*animationSpeed;
 
-    image.position.set(newPosition.x, newPosition.y,0);
+    array[i].style.left = newPosition.x + "px";
+    array[i].style.top = newPosition.y + "px";
 }
 
-function Raycast(color)
-{
-    MousePosition();
-    raycaster.setFromCamera( mouse.clone(), camera );   
-
-    var objects = raycaster.intersectObjects(sprite);
-    var num = -1;
-
-    for(i=0; i < sprite.length; i++)
-    {
-        sprite[i].material.color.set("white");
-    }
-
-    for(i = 0; i < objects.length; i++)
-    {
-        objects[i].object.material.color.set( color );
-
-        for(o = 0; o < sprite.length; o++)
-        {
-            if(objects[i].object.id == sprite[o].id)
-            {
-                num = o;
-            }
-        }
-    }
-
-    return num;
-}
-
+/*
 function OnMouseDown()
 {
     mouseDown = true;
@@ -237,84 +200,137 @@ function OnMouseUp()
 function OnMouseMove()
 {
     Raycast("rgb(255, 0, 0)");
-}
+}*/
 
-function CreateText(){
-    var loader = new THREE.FontLoader();
-    loader.load( 'Fonts/helvetiker_regular.typeface.json', function ( font ) {
+function CreateText(i){
+    var button = document.createElement("button");
+    button.innerHTML = sentences[i];
+    button.style.fontSize = 40;
 
-        var geometry = new THREE.TextGeometry( 'Hello three.js!', 
-                                              {
-            font: font,
-            size: 1,
-            height: 0.5,
-            curveSegments: 4,
-            bevelEnabled: true,
-            bevelThickness: 0.02,
-            bevelSize: 0.05,
-            bevelSegments: 3
-        } ); 
-        geometry.center();
-        var material = new THREE.MeshBasicMaterial();
-        material.color.set("white");
-        var mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
-        text=mesh;
+    body.appendChild(button);
+    button.style.position = "absolute";
 
-    } );
+    AddHandler(button, i, false);
+
+    return button;
 }
 
 function EditText(text){
-
+    text.style.fontSize = text.offsetHeight/4;
 }
 
-function CreateAudios()
+function ButtonPress(i, isImage)
 {
-    // load a sound and set it as the Audio object's buffer
-    var audioLoader = new THREE.AudioLoader();
-    var audios=
-        [
-            'Sounds/English/Animals/ENhamahakki.mp3',
-            'Sounds/English/Animals/ENmarso.mp3',
-            'Sounds/English/Animals/ENhevonen.mp3',
-            'Sounds/English/Animals/ENhiiri.mp3',
-            'Sounds/English/Animals/ENkarme.mp3',
-            'Sounds/English/Animals/ENkala.mp3',
-            'Sounds/English/Animals/ENkani.mp3'
-        ]
+    sounds[i].play();
 
-    for(i = 0; i < audios.length; i++)
-    {
-        CreateAudio(i, audios, audioLoader);
-    }
-}
-
-function CreateAudio(i, audios, audioLoader)
-{
-    sounds[i] = new THREE.Audio( listener );
-
-    audioLoader.load( audios[i], function( buffer ) 
-                     {
-        sounds[i].setBuffer( buffer );
-    });
-}
-
-function RaycastText(geo){
-    var inverseMatrix = new THREE.Matrix4(), ray = new THREE.Ray();
-    //for example textGeo is the textGeometry
-    inverseMatrix.getInverse(geo.matrixWorld);
-    ray.copy(raycaster.ray).applyMatrix4(inverseMatrix);
-
-    if(geo.geometry.boundingBox !== null){
-        if(ray.intersectsBox(geo.geometry.boundingBox) === true){
-            geo.material.color.set("rgb(255,0,0)"); 
+    if(!isImage){
+        if(chosen == i){
+            animation = setInterval(Animation, 1);
         }
     }
 }
 
-function MousePosition(){
+
+
+function CreateAudios()
+{
+
+    for(i = 0; i < audios.length; i++)
+    {
+        CreateAudio(i, audios);
+    }
+}
+
+function CreateAudio(i, audios)
+{
+    sounds[i] = document.createElement("audio");
+    sounds[i].src = audios[i];
+}
+
+function CreateTexts(){
+    for(i = 0; i < sentences.length; i++){
+        texts.push(CreateText(i));
+    }
+}
+
+function CreateImages()
+{
+    for(i = 0; i < spriteMap.length; i++)
+    {
+        var button = document.createElement("input");
+
+        button.type = "image";
+
+        body.appendChild(button);
+        button.style.position = "absolute";
+        button.style.width = 100 + "px";
+        button.style.height = 100 + "px";
+
+        button.src = spriteMap[i]; 
+
+        AddHandler(button, i, true);
+
+        sprite[i] = button;
+
+        var newImg = new Image();
+        newImg.src = spriteMap[i];
+        images[i] = newImg;
+    }
+}
+
+function MousePosition()
+{
     var size = renderer.getSize(new THREE.Vector2);
     mouse.x = ( (event.clientX - border.x) /  size.x) * 2 - 1;
     mouse.y = -( (event.clientY - border.y) / size.y ) * 2 + 1;
     return mouse;
+}
+
+function ChooseRandom()
+{
+    for(i = 0; i < sprite.length; i++){
+        sprite[i].style.display = "none";
+        texts[i].style.display = "none";
+    }
+
+    random = [];
+    for(i = 0; i < optionsAmount; i++){
+        var num = -1
+        do{
+            num = Math.floor(Math.random() * sprite.length);
+        }while(random.includes(num));
+        random[i] = num;
+        texts[num].style.display = "block";
+    }
+
+    chosen = random[Math.floor(Math.random() * random.length)];
+    sprite[chosen].style.display = "block";
+}
+
+function AddHandler(button, i, isImage)
+{
+    button.addEventListener ("click", function() {
+        ButtonPress(i, isImage);
+    });
+}
+
+var times = 0;
+const maxTimes=300;
+var animationDone = false;
+function Animation(){
+    times++;
+    animationX -= Math.sin(times/maxTimes);
+    Resize();
+    if(times>maxTimes){
+        if(!animationDone){
+            animationDone=true;
+            animationX*=-1;
+            ChooseRandom();
+        }else{
+            animationDone=false;
+            clearInterval(animation);
+            ButtonPress(chosen, true);
+        }
+        times = 0;
+    }
 }
